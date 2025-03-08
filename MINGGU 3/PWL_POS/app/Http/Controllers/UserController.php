8303b3c108b8b8c4b335a82dff3f44983937f5f0
RizkyRR27 <rizkyroza2005@gmail.com>
2025-03-08 14:09:39 +0700

    komit

--- MINGGU 3/PWL_POS/app/Http/Controllers/UserController.php
+++ MINGGU 3/PWL_POS/app/Http/Controllers/UserController.php
@@ -10,6 +10,22 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index() {
+        // tambah data user dengan eloquent model
+        $data = [
+            'level_id' => 2,
+            'username' => 'manager_tiga',
+            'nama' => 'Manager 3',
+            'password' => Hash::make('12345'),
+            
+        ];
+        UserModel::create($data);
+
+        $user = UserModel::all();
+        return view('user', ['data' => $user]);
+
+
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
         // //coba akses usermodel
         // $user = UserModel::all(); //ambil semua dari tabel m_user
         // return view('user', ['data' => $user]);
@@ -26,13 +42,13 @@ class UserController extends Controller
         // $user = UserModel::all();
         // return view('user', ['data' => $user]);
 
-        $data = [
-            'nama' => "Pelanggan Pertama",
-        ];
-        UserModel::where('username', 'customer-1')->update($data);
+        // $data = [
+        //     'nama' => "Pelanggan Pertama",
+        // ];
+        // UserModel::where('username', 'customer-1')->update($data);
 
-        $user = UserModel::all();
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all();
+        // return view('user', ['data' => $user]);
 
     }
 }
